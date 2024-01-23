5fda2223c0d21a2588247d7818047dadfc19574b
Piescu47 <149490754+Piescu47@users.noreply.github.com>
2024-01-23 11:45:34 +0000

    Commit 23

---
+++ ejercicios_html/js-ejerc01-primer_programa.js
@@ -0,0 +1,4 @@
+ alert("Esto lo ejecuto desde un archivo externo!") 
+alert ( "CR7 best jugador ever")
+let x = 47; // Definición de variable x, da le valor de 47
+let y = x + 1; /* Definición de variable y, da le valor de x + 1, aplicable a la escala anterior*/
